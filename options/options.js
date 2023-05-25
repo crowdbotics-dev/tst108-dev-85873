fba12fb7faebab43c364e1f810ed7c2db239d1b4
Penny Wanjiru <penina@crowdbotics.com>
2023-05-25 16:38:53 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tst108-dev-85873",
-  url: "https://tst108-dev-85873.botics.co",
-  api: "https://tst108-dev-85873.botics.co/api/v1"
-}
+  "name": "tst108-dev-85873",
+  "url": "https://tst108-dev-85873.botics.co",
+  "api": "https://tst108-dev-85873.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
